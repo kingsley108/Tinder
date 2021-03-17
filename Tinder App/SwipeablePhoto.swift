935eb60a49c2a5495e059c937dded318540b5b67
Kinglsey Charles <Kingsley.charles@global.com>
2021-03-17 08:26:13 +0000

    EP 5: Rotational Transformation and Card Dismiss

--- Tinder App/SwipeablePhoto.swift
+++ Tinder App/SwipeablePhoto.swift
@@ -36,21 +36,41 @@ class SwipeablePhoto: UIView {
     @objc func handleGesture(gesture: UIPanGestureRecognizer) {
         
         let translation = gesture.translation(in: nil)
+        let degrees = (translation.x / 20) * CGFloat.pi / 180
         
         switch gesture.state {
         case .changed:
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+            self.transform = CGAffineTransform(rotationAngle: degrees).translatedBy(x: translation.x, y: translation.y)
+            print(translation.x)
+            
         case .ended:
-            animateEnded()
+            animateEnded(animatedby: Int(translation.x), gesture: gesture)
         default:
             break
         }
         
     }
     
-    func animateEnded() {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.1, options: .curveEaseOut) {
+    func animateEnded(animatedby: Int, gesture: UIPanGestureRecognizer ) {
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut) {
+            if animatedby > 100 || animatedby < -100 {
+                self.center = CGPoint(x: 1000 * translationDirection, y: 0)
+            }
+            else {
+                self.transform = .identity
+            }
+            
+        } completion: { (_) in
+            self.frame = CGRect(x: 0,
+                                y: 0,
+                                width: self.superview!.frame.width,
+                                height: self.superview!.frame.height)
             self.transform = .identity
+            
         }
+        
+        
+        
     }
 }
